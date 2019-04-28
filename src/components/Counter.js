d38bb218e618df6673291a65c9af51d1f6525a3e
sinamoraddar <sinamoraddar@gmail.com>
2019-04-28 17:51:36 +0430

    version 1

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,14 +1,52 @@
 import React from 'react';
 import ChangeNumberButton from './ChangeNumberButton';
 
-const Counter = () => (
-    <div>
-        <div>testing</div>
-        <ChangeNumberButton/>
-        <ChangeNumberButton/>
-        <ChangeNumberButton/>
-    </div>
+class Counter extends React.Component {
+    state = {
+        count: 0,
+        buttonType:
+        {
+            add: 'add',
+            subtract: 'subtract',
+            reset: 'reset'
+        }
+    }
+    //handling the count state based on the clicked button
+    handleChange = (changeType) => {
+        const { changeAmount, resetAmount } = this.props;
+        const { count } = this.state;
+        switch (changeType) {
+            case 'add':
+                this.setState(() => ({
+                    count: count + changeAmount
+                }))
+                break;
+            case 'subtract':
+                this.setState(() => ({
+                    count: count - changeAmount
+                }))
+                break;
+            case 'reset':
+                this.setState(() => ({
+                    count: resetAmount
+                }))
+                break;
+            default:
+                return undefined
+        }
 
-);
+    }
+    render() {
+        return (
+            <div>
+                <div>{this.state.count}</div>
+                <ChangeNumberButton handleChange={this.handleChange} buttonType={this.state.buttonType.add} />
+                <ChangeNumberButton handleChange={this.handleChange} buttonType={this.state.buttonType.subtract} />
+                <ChangeNumberButton handleChange={this.handleChange} buttonType={this.state.buttonType.reset} />
+            </div>
+        )
+    }
+
+};
 
 export default Counter;
--- src/components/Settings.js
+++ src/components/Settings.js
@@ -1,7 +1,22 @@
 import React from 'react';
-
-const Settings = () => (
-    <div>settings</div>
+//settings file for manipulating the amount which our data is being changed by
+const Settings = (props) => (
+    <div>
+        change amount by:
+         <input
+            type="number"
+            value={props.changeAmount}
+            name="changeAmount"
+            onChange={(e) => props.handleChange(e.target.name, e.target.value)}
+        />
+        reset amount to:
+        <input
+            type="number"
+            value={props.resetAmount}
+            name="resetAmount"
+            onChange={(e) => props.handleChange(e.target.name, e.target.value)}
+        />
+    </div>
 );
 
 export default Settings;
